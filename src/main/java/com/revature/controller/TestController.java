0bfd5ffcde8666e0bd0599ed8fc5852369d642ca
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-07-07 17:35:41 -0400

    added swagger, swagger-ui, swagger-bean-validator

--- src/main/java/com/revature/controller/TestController.java
+++ src/main/java/com/revature/controller/TestController.java
@@ -1,8 +1,21 @@
 package com.revature.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
+@RequestMapping("/test")
+@CrossOrigin(origins = "*", 
+methods = {RequestMethod.GET, RequestMethod.PUT, 
+			RequestMethod.PATCH, RequestMethod.POST},
+allowedHeaders = {"*"})
 public class TestController {
-
+	@PostMapping
+	public String test() {
+		return "hello world";
+	}
 }
