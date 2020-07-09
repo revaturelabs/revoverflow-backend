f7f188a7a2a327f13af9cfd530e1a458fad3f88b
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2020-07-09 10:04:55 -0700

    Just added comments in the Controller and Server file of RSS

--- src/main/java/com/revature/controller/RSSController.java
+++ src/main/java/com/revature/controller/RSSController.java
@@ -2,4 +2,12 @@ package com.revature.controller;
 
 public class RSSController {
 
+	/*
+	 * Add Points - Kei
+	 * 
+	 * Update points - Haji
+	 * 
+	 * Login - Ryan
+	 * 
+	*/
 }
--- src/main/java/com/revature/services/RSSService.java
+++ src/main/java/com/revature/services/RSSService.java
@@ -2,4 +2,11 @@ package com.revature.services;
 
 public class RSSService {
 
+	/*
+	 * Add Points - Kei
+	 * 
+	 * Update Points - Haji
+	 * 
+	 * Login - Ryan
+	*/
 }
