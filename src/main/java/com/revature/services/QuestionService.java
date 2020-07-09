67a09b6becd374dc89d8524237c83934ee5232c2
kRenelt <43582922+kRenelt@users.noreply.github.com>
2020-07-09 15:44:26 -0500

    added a getAllQuestion for the QuestionController and the QuestionService

--- src/main/java/com/revature/services/QuestionService.java
+++ src/main/java/com/revature/services/QuestionService.java
@@ -1,8 +1,20 @@
 package com.revature.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import com.revature.entities.Questions;
+import com.revature.repositories.QuestionRepository;
 
 @Service
 public class QuestionService {
 
+	@Autowired
+	QuestionRepository questionRepository;
+	
+	public Page<Questions> getAllQuestions(Pageable pageable){
+		return questionRepository.findAll(pageable);
+	}
 }
