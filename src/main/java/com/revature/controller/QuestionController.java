673eb322f5d43036687ab7353ded3e2d2fd8d62b
James <Walls>
2020-07-09 17:17:37 -0400

    Additions to QuestionController

--- src/main/java/com/revature/controller/QuestionController.java
+++ src/main/java/com/revature/controller/QuestionController.java
@@ -1,8 +1,28 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.revature.entities.Questions;
+import com.revature.services.QuestionService;
 
 @RestController
+@RequestMapping("/Question")
 public class QuestionController {
+	
+	@Autowired
+	QuestionService questionService;
+	
+	@GetMapping
+	
+	
+	@PostMapping
+	public Questions saveQuestion(@Valid @RequestBody Questions question) {
+		return questionService.save(question);
+	}
+	
 
 }
