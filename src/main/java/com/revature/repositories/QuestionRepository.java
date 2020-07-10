b0b215ba91917dadef02e73fa4c88a71664ba155
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-07-10 17:42:22 -0400

    Added update questions by answerid to question repository

--- src/main/java/com/revature/repositories/QuestionRepository.java
+++ src/main/java/com/revature/repositories/QuestionRepository.java
@@ -3,6 +3,9 @@ package com.revature.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.revature.entities.Question;
@@ -15,4 +18,10 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 
 	/**@author ken*/
 	Page<Question> getAllQuestionsByStatus(Pageable pageable, boolean status);
+	
+//public interface QuestionRepository extends JpaRepository<Questions, Integer>{
+//	@Modifying(clearAutomatically = true)
+//	@Query("UPDATE Questions q SET q.accepted_answer_id = :accepted_answer_id WHERE q.id = :id")
+//	int updateQuestionAcceptedAnswerId(@Param("id") int id, @Param("accepted_answer_id") int accepted_answer_id);
+////	Page<Questions> 
 }
