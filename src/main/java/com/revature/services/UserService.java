7627d15e8331dacad776f72b2e0be8d9c3f2bb01
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-07-11 17:34:11 -0400

    added get all user endpoint, finalized code for login service

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,8 +1,22 @@
 package com.revature.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.revature.entities.User;
+import com.revature.repositories.UserRepository;
 
 @Service
 public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	public List<User> getAllUsers() {
+		
+		return userRepository.findAll();
+	}
 
 }
