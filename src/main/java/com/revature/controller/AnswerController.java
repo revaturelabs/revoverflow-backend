3d01bf16769136c945614cf4700b5e8cfbc722d8
James <Walls>
2020-07-11 22:24:24 -0400

    Adding PostAnswer methods

--- src/main/java/com/revature/controller/AnswerController.java
+++ src/main/java/com/revature/controller/AnswerController.java
@@ -1,8 +1,23 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.revature.entities.Answer;
+import com.revature.services.AnswerService;
 
 @RestController
+@RequestMapping("/answers")
 public class AnswerController {
+	
+	@Autowired
+	AnswerService answerService;
+	
+	@PostMapping
+	public Answer saveAnswer(Answer answer) {
+		return answerService.save(answer);
+	}
 
 }
--- src/main/java/com/revature/services/AnswerService.java
+++ src/main/java/com/revature/services/AnswerService.java
@@ -1,8 +1,19 @@
 package com.revature.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.revature.entities.Answer;
+import com.revature.repositories.AnswerRepository;
 
 @Service
 public class AnswerService {
+	
+	@Autowired
+	AnswerRepository answerRepository;
+	
+	public Answer save(Answer answer) {
+		return answerRepository.save(answer);
+	}
 
 }
