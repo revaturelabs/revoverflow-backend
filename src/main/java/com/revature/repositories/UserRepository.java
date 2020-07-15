4a7035e1c4630de529527bbd9c25717d75cc8981
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-07-15 11:50:23 -0400

    security is almost working keyword almost

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import com.revature.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+	User findByEmail(String string);
+
 }
--- src/main/java/com/revature/util/JwtRequestFilter.java
+++ src/main/java/com/revature/util/JwtRequestFilter.java
@@ -8,8 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ResourceProperties.Chain;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.revature.entities.User;
+import com.revature.repositories.UserRepository;
+import com.revature.services.UserService;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
@@ -17,17 +26,30 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	@Autowired
 	private JwtUtil jwtUtil;
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
-		final String authHeader = request.getHeader("Authorization");
-		String username = null;
-		String jwt = null;
+		final String jwt = request.getHeader("Authorization");
+		String email = null;
 		
-		if (authHeader != null) {
-			
+		if (jwt != null) {
+			email = jwtUtil.extractEmail(jwt);
+			System.out.println(email);
 		}
+		if (email != null && SecurityContextHolder.getContext().getAuthentication()==null) {
+			User user = userRepository.findByEmail(email);
+			if(jwtUtil.validateToken(jwt, user)) {
+				System.out.println("valid");
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(user, null);
+				usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			}
+		}
+		filterChain.doFilter(request,response);
 	}
 
 }
