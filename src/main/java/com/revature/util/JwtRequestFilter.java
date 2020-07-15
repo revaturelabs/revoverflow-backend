72810550fe626a683b95dcdda7b96bcc9e81e5d2
Rclayton999 <64425715+Rclayton999@users.noreply.github.com>
2020-07-15 10:42:58 -0400

    added webSecurityConfig

---
+++ src/main/java/com/revature/util/JwtRequestFilter.java
@@ -0,0 +1,33 @@
+package com.revature.util;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		final String authHeader = request.getHeader("Authorization");
+		String username = null;
+		String jwt = null;
+		
+		if (authHeader != null) {
+			
+		}
+	}
+
+}
