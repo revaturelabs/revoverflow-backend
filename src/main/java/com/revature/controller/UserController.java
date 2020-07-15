2c844691b814d82d1b8dae29115c64abd1f83368
James <Walls>
2020-07-15 12:02:35 -0400

    Added get by ID method for users

--- src/main/java/com/revature/controller/UserController.java
+++ src/main/java/com/revature/controller/UserController.java
@@ -1,8 +1,25 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.revature.entities.User;
+import com.revature.services.UserService;
+
+@RequestMapping("/users")
 @RestController
 public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	/** @Author James Walls */
+	@GetMapping("/{userID}")
+	public User getUserInformationById(@PathVariable int userID) {
+		return userService.findById(userID);
+	}
 
 }
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,8 +1,23 @@
 package com.revature.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.revature.entities.User;
+import com.revature.repositories.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	/** @Author James Walls */
+	public User findById(int userID) {
+		return userRepository.findById(userID)
+					.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+	}
 
 }
