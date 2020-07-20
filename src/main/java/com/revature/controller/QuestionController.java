46142dfc934bac9aecdc9ef9c3ebb11f680b99ad
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-07-19 21:12:37 -0400

    Cleaned up QuestionController

--- src/main/java/com/revature/controller/QuestionController.java
+++ src/main/java/com/revature/controller/QuestionController.java
@@ -7,14 +7,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,11 +22,6 @@ import com.revature.services.QuestionService;
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
-
-	@ModelAttribute
-	LocalDate creationDate() {
-		return LocalDate.now();
-	}
 	
 	@Autowired
 	QuestionService questionService;
@@ -83,7 +75,6 @@ public class QuestionController {
 	/** @author Hugh Thornhill */
 	@GetMapping("/recent")
 	public Page<Question> findAllByOrderByCreationDateDesc(Pageable creationDatePageable){
-//		System.out.println(creationDate);
 		return questionService.findAllByOrderByCreationDateDesc(creationDatePageable);
 	}
 }
