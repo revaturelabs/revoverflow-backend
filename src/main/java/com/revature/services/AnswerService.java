1e6e6e98ec889556697dd136f28cc6bd365afe10
Natasha <Nmposer4927@gmail.com>
2020-07-24 14:01:20 -0400

    Documentation added

--- src/main/java/com/revature/services/AnswerService.java
+++ src/main/java/com/revature/services/AnswerService.java
@@ -3,8 +3,9 @@ package com.revature.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import com.revature.entities.Answer;
 import com.revature.repositories.AnswerRepository;
@@ -15,7 +16,8 @@ public class AnswerService {
  @Autowired
  AnswerRepository answerRepository;
  
-  /** @Author Natasha Poser */
+  /** @Author Natasha Poser 
+   * @return retrieves all answers matching a specific question ID*/
  public Page<Answer> getAnswerByQuestionId(Pageable pageable, int questionId){
 	 return answerRepository.getAnswerByQuestionId(pageable, questionId);
  }
@@ -32,21 +34,25 @@ public class AnswerService {
 		return answerRepository.getAllAnswersByUserId(pageable, id);		
 	}
 
-	/** @author Natasha Poser */
+	/** @author Natasha Poser 
+	 * @return retrieves all answers in database*/
 	 public Page<Answer> getAnswers(Pageable pageable) {
 		 return answerRepository.findAll(pageable);
 	 }
 	
-	/** @author Natasha Poser */ 
+	/** @author Natasha Poser 
+	 * @return retrieves the accepted answers accosiated with a specific Question Id*/ 
 	public Page<Answer> getAcceptedAnswerByQuestionId(Pageable pageable, int acceptedId){
 		return answerRepository.getAcceptedAnswerByQuestionId(pageable, acceptedId);
 	}
 	
-	/** @author Natasha Poser */
-	public Page<Answer> getAnswerById(Pageable pageable, int id){
-		return answerRepository.getAnswerById(pageable, id);
+	/** @author Natasha Poser 
+	 * @return retrieves a specific Answer by using it's ID*/
+	public Answer findById(int id) {
+		return answerRepository.findById(id)
+				// If no answer is found by the particular ID then HTTP Status is given 
+				.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
 	}
-
 	
 }
 
