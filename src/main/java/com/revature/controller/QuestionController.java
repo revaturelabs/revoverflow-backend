c017bff9fdb9c76d872e7dfd76069f4a72e496df
chris castaneda <chris.j.castaneda@gmail.com>
2020-07-29 15:38:11 -0700

    code smell fix

--- src/main/java/com/revature/controller/QuestionController.java
+++ src/main/java/com/revature/controller/QuestionController.java
@@ -1,6 +1,6 @@
 package com.revature.controller;
 
-import javax.validation.Valid;
+import javax.validation.Valid; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
