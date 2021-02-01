17586f73503a6a5c5d69c67804fc1c5fad74ae82
Mahesh <kallemahesh123@gmail.com>
2021-02-01 10:13:00 -0600

    updated faq repository

--- src/main/java/com/revature/repositories/FaqRepository.java
+++ src/main/java/com/revature/repositories/FaqRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.revature.entities.Faq;
@@ -21,9 +22,9 @@ public interface FaqRepository extends JpaRepository<Faq, Integer>{
 	public List<Faq> getRevatureBasedFaq();
 	
 	@Query("From Faq f INNER JOIN Question q ON f.id = q.id WHERE q.locationID = :location")
-	public List<Faq> getFaqByLocation(int location);
+	public List<Faq> getFaqByLocation(@Param(value = "location") int location);
 
 	@Query("From Faq f INNER JOIN Question q ON f.id = q.id WHERE q.revatureQuestion=true AND q.locationID = :location")
-	public List<Faq> getRevatureAndLocationFaq(int location);
+	public List<Faq> getRevatureAndLocationFaq(@Param(value = "location") int location);
 
 }
